c913dd5e50f548986be3d670ac667f1d90ac1b76
buzzBoy21 <buzzBoy021@gmail.com>
2024-10-27 02:10:32 +0200

    add font and make button

--- frontend/src/renderer/src/App.jsx
+++ frontend/src/renderer/src/App.jsx
@@ -1,13 +1,27 @@
-import Header from './components/Header/Header';
+import { useState } from 'react';
 import KeyBoard from './components/Keyboard/KeyBoard';
 import TemplateKeyBoardContext from './context/templateKeyBoardContext';
-
+import style from './App.module.css';
+import Button from './components/button/button';
 function App() {
+   const [step, setStep] = useState(0);
+   let contentPage;
+   if (step == 0) {
+      contentPage = (
+         <>
+            <h1 className={style.primero}>Select your new hot keys</h1>
+            <KeyBoard />
+         </>
+      );
+   }
+   const handleButton = () => {
+      setStep((prevValue) => ++prevValue);
+   };
    return (
       <>
-         <Header></Header>
          <TemplateKeyBoardContext>
-            <KeyBoard />
+            {contentPage}
+            <Button onClick={handleButton}>Next step </Button>
          </TemplateKeyBoardContext>
       </>
    );
---
+++ frontend/src/renderer/src/components/button/button.jsx
@@ -0,0 +1,52 @@
+import PropTypes from 'prop-types';
+import style from './button.module.css';
+import { StyleSheet, css } from 'aphrodite';
+
+export default function Button({
+   children,
+   onClick,
+   paddingTopAndBottom = null, //can use any unit
+   fontSize = '1em', //can use any unit and the words: large, ect
+   widthPerLetter = '12', //this is px
+   animationDuration = null,
+}) {
+   const allLetter = children.split('');
+   const allLetterDelay = {};
+
+   //when animation is playing the width doen't change
+   const widthInPx = allLetter.length * widthPerLetter;
+
+   for (let index = 0; index < allLetter.length; index++) {
+      const key = 'delayAnimation' + index;
+      allLetterDelay[key] = {
+         'animation-delay': `${index * 100}ms`,
+         fontSize: fontSize,
+         animationDuration: animationDuration && animationDuration, //If animationDuration is null aphrodite will not use it
+      };
+   }
+   const styles = StyleSheet.create(allLetterDelay);
+
+   return (
+      <button
+         onClick={onClick}
+         className={style['button-style']}
+         style={
+            paddingTopAndBottom
+               ? { padding: `${paddingTopAndBottom} 0`, width: `${widthInPx}px` }
+               : { width: `${widthInPx}px` }
+         }
+      >
+         {allLetter.map((letter, index) => (
+            <span key={index} className={css(styles[`delayAnimation${index}`])}>
+               {letter}
+            </span>
+         ))}
+      </button>
+   );
+}
+
+Button.propTypes = {
+   children: PropTypes.string,
+   onClick: PropTypes.func,
+   padding: PropTypes.string,
+};
